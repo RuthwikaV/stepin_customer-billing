0fd6d509a06343b8ef3fb3cad6774caab03c2435
RuthwikaV <89633636+RuthwikaV@users.noreply.github.com>
2021-09-08 23:15:39 +0530

    Add files via upload

---
+++ Implementation/main.c
@@ -0,0 +1,66 @@
+/**
+ * @file main.c
+ * @author your name Ruthwika(ruthwika.vos@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include<stdio.h>
+ #include<conio.h>
+ #include<stdlib.h>
+
+
+void main()
+	{
+	  int i,n;
+	  char ch;
+	  clrscr();
+
+	  _setcursortype(_NOCURSOR);
+	  printf("   CUSTOMER BILLING SYSTEM:\n\n");
+	  printf("===============================\n");
+	  printf("\n1:    to add account on list\n");
+	  printf("2:    to search customer account\n");
+	  printf("3:    exit\n");
+	  printf("\n================================\n");
+	  do{
+	       printf("\nselect what do you want to do?");
+	       ch=getche();
+	  }while(ch<='0' || ch>'3');
+	  switch(ch){
+		case '1':
+			clrscr();
+			printf("\nhow many customer accounts?");
+			scanf("%d",&n);
+			for(i=0;i<n;i++){
+				input();
+				if(customer.payment>0)
+					customer.acct_type=(customer.payment<0.1*customer.oldbalance)? 'O': 'D';
+				else
+					customer.acct_type=(customer.oldbalance>0)?'D' : 'C';
+				customer.newbalance=customer.oldbalance - customer.payment;
+				writefile();
+			}
+			main();
+		case '2':
+			clrscr();
+			printf("search by what?\n");
+			printf("\n1 --- search by customer number\n");
+			printf("2 --- search by customer name\n");
+			search();
+			ch=getche();
+			main();
+		case '3':
+			clrscr();
+			delay(700);
+			textcolor(RED);
+			gotoxy(25,25);
+			cprintf("\nA PROJECT BY BIDUR & SUJAN");
+			delay(1500);
+			exit(1);
+	  }
+ }
